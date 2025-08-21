0f81685c1add071309e1f1f1a4126482c245d0a4
Melnik George <melnik.goshan2013@gmail.com>
2025-08-21 20:51:20 +0300

    Ui fixes

--- src/components/ui/error-alert.tsx
+++ src/components/ui/error-alert.tsx
@@ -2,13 +2,14 @@ import { ContractCall } from '@/lib/simulation';
 import { Alert, AlertDescription, AlertTitle } from './alert';
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 import { ContractCallSignature } from './signature';
+import { useSettings } from '@/lib/context/settings-context-provider';
 
 const ErrorAlert = ({ contractCallError }: { contractCallError: ContractCall | undefined }) => {
 	const errorDescription = contractCallError?.errorMessage;
 	return (
-		<Alert className="w-fit my-2 border-red-600  dark:text-white">
+		<Alert variant="compact" className="w-fit my-2 border-red-600 dark:text-white">
 			<ExclamationTriangleIcon className="h-5 w-5 !text-red-600" color="red" />
-			<AlertTitle className="">Error message: </AlertTitle>
+			<AlertTitle className="!font-light">Error message: </AlertTitle>
 			<AlertDescription>
 				<span className="text-red-600">{errorDescription}</span> in{' '}
 				{contractCallError && <ContractCallSignature contractCall={contractCallError} />}
