e9e86e6f52ee5e302d8d130244a9861321ee0336
Melnik George <melnik.goshan2013@gmail.com>
2025-12-18 22:22:31 +0400

    Fixed error alert

--- src/components/ui/error-alert.tsx
+++ src/components/ui/error-alert.tsx
@@ -5,13 +5,14 @@ import { ContractCallSignature } from './signature';
 import { FnName } from './function-name';
 
 const ErrorAlert = ({ callError }: { callError: ContractCall | FunctionCall | undefined }) => {
-	const errorDescription = callError?.errorMessage;
+	const errorDescription = callError?.errorMessage?.replaceAll('\u0000', '');
 	return (
 		<Alert variant="compact" className="w-fit my-2 border-red-600 dark:text-white">
 			<ExclamationTriangleIcon className="h-5 w-5 !text-red-600" color="red" />
 			<AlertTitle className="!font-light">Error message: </AlertTitle>
 			<AlertDescription>
-				<span className="text-red-600">{errorDescription}</span> in{' '}
+				<span className="text-red-600">{errorDescription}</span>
+				{'  '}in{'  '}
 				{callError && 'classHash' in callError ? (
 					<ContractCallSignature contractCall={callError} />
 				) : (
